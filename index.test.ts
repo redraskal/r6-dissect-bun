820da2dd4631780c9cd6a4088ed66675d7d8052a
redraskal <ben@ryben.dev>
2023-07-19 02:45:02 -0500

    fix: reading matches, add more tests

--- index.test.ts
+++ index.test.ts
@@ -1,10 +1,16 @@
 import { test, expect } from "bun:test";
-import { dissect } from ".";
+import { dissect, dissectMatch } from ".";
 
-test("can read test.rec", async () => {
-	const { data, error } = await dissect("test.rec");
+test("can read a replay", async () => {
+	const { data, error } = await dissect("replays/test.rec");
 	expect(error).toBeUndefined();
 	expect(data?.matchID).toBe("98ea92e1-2285-47a2-8e03-f17f96557640");
+});
+
+test("can read a match", async () => {
+	const { data, error } = await dissectMatch("replays/Match-2023-06-10_01-28-10-228");
+	expect(error).toBeUndefined();
+	expect(data?.rounds).toBeArrayOfSize(1);
 });
 
 test("cannot read non-existant file", async () => {
